f34041f7cb9a00f8a7cb7ed8c3a70a42f879f758
ThayerDavid-FS <dathayer@student.fullsail.edu>
2026-09-11 16:12:39 -0500

    Refactor author controller methods to use async/await and improve error handling

--- app/controller/authorController.js
+++ app/controller/authorController.js
@@ -1,52 +1,86 @@
 
 const Author = require("../models/author")
 
-const getAllAuthors =  (req, res) => {
-    res
+const getAllAuthors = async (req, res) => {
+    try {
+        const authors = await Author.find({});
+           res
         .status(200)
-        .json({ success: true, message: `${req.method} - Request received to authors endpoint` });
+        .json({ success: true, data: authors });
     }
+        
+    catch (error) {
+           res
+        .status(400)
+        .json({ success: false, message: error.message });
+    }
+};
 
-const getAuthorById = (req, res) => {
+const getAuthorById = async (req, res) => {
 
-const {id} = req.params;
-    res
-    .status(200)
-    .json({
-        id, 
-        success: true, 
-        message: `${req.method} - Request received to authors endpoint` });
+try {
+    const {id} = req.params;
+    const author = await Author.findById(id);
+
+    if (!author) {
+        return res.status(404).json({ success: false, message: 'Author not found!'})
+    }
+        res
+        .status(200)
+        .json({ success: true, data: author });
+} catch (error) {
+    res.status(400).json({success: false, message: "Access denied"})
+    
+}
 }
 
 const createAuthor = async (req, res) => {
+    
     try {
         const author = await Author.create(req.body)
     res
-    .status(200)
-    .json({ success: true, message: `${req.method} - Request received to authors endpoint` });
-    } catch (error) {res
+    .status(201)
+    .json({ success: true, data: author });
+    } catch (error) {
+        res
     .status(400)
-    .json({ success: false, message: `${req.method} - Request denied to authors endpoint` });
+    .json({ success: false, message: `${req.method} - Request denied` });
     }
     
 };  
 
-const updateAuthor = (req, res) => {
-    const {id} = req.params;
-    res
-    .status(200)
-    .json({
-        id,
-        success: true,
-        message: `${req.method} - Request received to authors endpoint` });
-};
+const updateAuthor = async (req, res) => {
+  try {
+      const {id} = req.params;
+      const author = await Author.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      if (!author) {
+        return res.status(404).json({ success: false, message: 'Author not updated!' })
+      }
+      res.status(200).json({success: true, data: author})
+  } catch (error) {
+    res.status(400)
+    .json({ success: false, message: error.message})
+}
+}
 
-const deleteAuthor = (req, res) => {
+const deleteAuthor = async (req, res) => {
+try {
     const {id} = req.params;
-    res
-        .status(200)
-        .json({ success: true, message: `${req.method} - Request received to authors endpoint` });
-    };
+    const author = await Author.findByIdAndDelete(id);
+
+    if (!author) {
+        return res.status(404).json({success: false, message: 'Author not deleted!'})
+    }
+    res.status(200).json({success: true, message: "Author success deleted"})
+} catch (error) {
+
+    res.status(500).json({ success: false, message: error.message})
+}
+}
+
 
 module.exports = {
     getAllAuthors,
